f330cdb012c3307fd1254d11c76e23d917ea9795
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-03-10 14:14:30 -0500

    Initial Commit

---
+++ gameboy-cpu/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  gameboy-cpu
+//
+//  Created by Gabriel Lumbi on 2023-03-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gameboy-cpu/gameboy_cpuApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameboy_cpuApp.swift
+//  gameboy-cpu
+//
+//  Created by Gabriel Lumbi on 2023-03-10.
+//
+
+import SwiftUI
+
+@main
+struct gameboy_cpuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
