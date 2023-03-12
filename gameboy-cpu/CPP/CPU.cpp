1221726203f604156f8f02bc5bc7959e083e1090
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-03-11 22:15:29 -0500

    Implement basic CPU

---
+++ gameboy-cpu/CPP/CPU.cpp
@@ -0,0 +1,68 @@
+//
+//  CPU.cpp
+//  gameboy-cpu
+//
+//  Created by Gabriel Lumbi on 2023-03-10.
+//
+
+#include "CPU.hpp"
+
+void CPU::fetch(Memory& memory) {
+    opcode = read(memory, program_counter++);
+}
+
+void CPU::execute(Memory& memory) {
+    switch (opcode) {
+        case 0x00: // NOP
+            break;
+
+        case 0xF9: { // LD SP, (HL)
+            stack_pointer = register_HL();
+            cycle++;
+            break;
+        }
+
+        case 0x08: { // LD SP, (NN)
+            Memory::Byte nn_lsb = read(memory, program_counter++);
+            Memory::Byte nn_msb = read(memory, program_counter++);
+            Memory::Address write_location = word(nn_lsb, nn_msb);
+            write(memory, write_location, lsb(stack_pointer));
+            write(memory, write_location + 1, msb(stack_pointer));
+            break;
+        }
+
+        case 0xAF: { // XOR A
+            register_A = 0x00;
+            flag_Z = 1;
+            flag_N = 0;
+            flag_H = 0;
+            flag_C = 0;
+            break;
+        }
+
+        case 0x36: { // LD (HL), N
+            Memory::Byte n = read(memory, program_counter++);
+            write(memory, register_HL(), n);
+            break;
+        }
+
+        case 0x32: { // LD (HL-), A
+            write(memory, register_HL(), register_A);
+            set_register_HL(register_HL()-1);
+        }
+
+        default:
+            break;
+    }
+}
+
+Memory::Byte CPU::read(Memory& memory, Memory::Address address) {
+    Memory::Byte data = memory.read(address);
+    cycle++;
+    return data;
+}
+
+void CPU::write(Memory& memory, Memory::Address address, Memory::Byte data) {
+    memory.write(address, data);
+    cycle++;
+}
---
+++ gameboy-cpu/CPP/CPU.hpp
@@ -0,0 +1,56 @@
+//
+//  CPU.hpp
+//  gameboy-cpu
+//
+//  Created by Gabriel Lumbi on 2023-03-10.
+//
+
+#ifndef CPU_hpp
+#define CPU_hpp
+
+#include "Memory.hpp"
+
+class CPU {
+
+public:
+    using Flag = bool;
+    using CycleCount = unsigned int;
+
+public:
+    // Execution
+
+    CycleCount cycle = 0;
+    Memory::Byte opcode = 0x00;
+
+    // Registers
+
+    Memory::Address program_counter = 0x0000;
+    Memory::Address stack_pointer = 0xFFFE; // Default SP location
+    Memory::Byte register_A = 0x00;
+    Memory::Byte register_B = 0x00;
+    Memory::Byte register_C = 0x00;
+    Memory::Byte register_D = 0x00;
+    Memory::Byte register_E = 0x00;
+    Memory::Byte register_F = 0x00;
+    Memory::Byte register_H = 0x00;
+    Memory::Byte register_L = 0x00;
+
+    Memory::Word register_HL() const { return register_H << 8 | register_L; }
+    void set_register_HL(Memory::Word data) { register_H = data >> 8; register_L = data & 0xFF; };
+
+    Flag flag_Z = 0;
+    Flag flag_N = 0;
+    Flag flag_H = 0;
+    Flag flag_C = 0;
+
+public:
+    void fetch(Memory&);
+    void execute(Memory&);
+
+private:
+    Memory::Byte read(Memory&, Memory::Address);
+    void write(Memory&, Memory::Address, Memory::Byte);
+
+};
+
+#endif /* CPU_hpp */
